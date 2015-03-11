b2ac0be9a6f0eaf0b89832f6596917ceb31cfd46
HILAFADIDA1 <hilafadida11@gmail.com>
2015-03-11 09:39:58 +0200

    assignment 1 done

--- src/com/java/project/Java_ProjectServlet.java
+++ src/com/java/project/Java_ProjectServlet.java
@@ -6,6 +6,6 @@ import javax.servlet.http.*;
 public class Java_ProjectServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, Hila");
+		resp.getWriter().println("Hello world");
 	}
 }
